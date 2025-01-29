ba545bd9f703bc5356e98d416d17e03c752901d6
Nishadh <36278517-Nishadh@users.noreply.replit.com>
2025-01-29 22:29:59 +0000

    User checkpoint: Debug and resolve deployment issues with geomanagerweb app on Replit, updating dependencies and adding a health check endpoint.

---
+++ replit.nix
@@ -0,0 +1,26 @@
+{pkgs}: {
+  deps = [
+    pkgs.libgeotiff
+    pkgs.zlib
+    pkgs.netcdf
+    pkgs.libjpeg
+    pkgs.hdf5
+    pkgs.curl
+    pkgs.postgis
+    pkgs.postgresql
+    pkgs.geos
+    pkgs.proj
+    pkgs.gdal
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
+    pkgs.yarn
+  ];
+}
