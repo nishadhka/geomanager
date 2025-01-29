359d2df951caf1351e9d122b1e091c7ef53dc186
Your Name <you@example.com>
2025-01-29 20:36:44 +0000

    copied the geomanger sandbox
    from https://github.com/erick-otenyo/geomanager/commit/50aeca5dcab949c926c44f2b9212a694b132013b

--- geomanagerweb/settings/dev.py
+++ geomanagerweb/settings/dev.py
@@ -1,27 +1,21 @@
 from .base import *
-import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v%m=de1@h_(n$$m!=h_v$egike=!!plq3=53(e3xlnk6qsu=fv"
+SECRET_KEY = "django-insecure-od#(q8@gly39*2_to74w6eg78_5@*y53%w*tvgo0yvuenv-_t="
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+# used in dev with Mac OS
+GDAL_LIBRARY_PATH = env.str('GDAL_LIBRARY_PATH', None)
+GEOS_LIBRARY_PATH = env.str('GEOS_LIBRARY_PATH', None)
+
 try:
     from .local import *
 except ImportError:
     pass
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-INSTALLED_APPS += ['wagtail.contrib.styleguide']
-
-# MapViewer configuration
-MAPVIEWER_BASE_PATH = os.getenv('MAPVIEWER_BASE_PATH', '/mapviewer')
-MAPVIEWER_ASSET_PREFIX = os.getenv('MAPVIEWER_ASSET_PREFIX',
-                                   '/mapviewer/static')
--- geomanagerweb/settings/production.py
+++ geomanagerweb/settings/production.py
@@ -1,37 +1,8 @@
-import os
-import secrets
-from decouple import config
 from .base import *
+
+DEBUG = False
 
 try:
     from .local import *
 except ImportError:
     pass
-
-WAGTAIL_ENABLE_UPDATE_CHECK = False
-
-SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe(64))
-
-ALLOWED_HOSTS = [
-    '*', 'https://*.replit.app', '.repl.co', '.replit.dev', '0.0.0.0'
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://*.repl.co', 'https://*.replit.dev', 'https://*.replit.app'
-] + config('CSRF_TRUSTED_ORIGINS', default='').split(',')
-
-DEBUG = False
-SECURE_CROSS_ORIGIN_OPENER_POLICY = config(
-    "SECURE_CROSS_ORIGIN_OPENER_POLICY", default=None)
-
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='').split(',')
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'unique-snowflake',
-    }
-}
-
-WAGTAIL_CACHE = False
