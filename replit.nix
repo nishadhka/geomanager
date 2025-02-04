e9651e581455848e093fa383a40ecb65b52b34a4
Nishadh KA <36278517-Nishadh@users.noreply.replit.com>
2025-02-04 05:51:24 +0000

    shellhook method to add the GDAL so library for django
    but it is not working

--- replit.nix
+++ replit.nix
@@ -1,26 +1,40 @@
-{pkgs}: {
-  deps = [
-    pkgs.libgeotiff
-    pkgs.zlib
-    pkgs.netcdf
-    pkgs.libjpeg
-    pkgs.hdf5
-    pkgs.curl
-    pkgs.postgis
-    pkgs.postgresql
-    pkgs.geos
-    pkgs.proj
-    pkgs.gdal
-    pkgs.tk
-    pkgs.tcl
-    pkgs.qhull
-    pkgs.pkg-config
-    pkgs.gtk3
-    pkgs.gobject-introspection
-    pkgs.ghostscript
-    pkgs.freetype
-    pkgs.ffmpeg-full
-    pkgs.cairo
-    pkgs.yarn
-  ];
+{ pkgs }: {
+    deps = [
+      pkgs.python39
+      pkgs.gdal
+      pkgs.geos
+      pkgs.proj
+      (pkgs.writeShellScriptBin "setup-gdal" ''
+        mkdir -p $HOME/.local/lib
+        ln -sf ${pkgs.gdal}/lib/libgdal.so $HOME/.local/lib/libgdal.so.30
+      '')
+    ];
+
+  env = {
+    # Build a library search path using Nix-built libraries.
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      "${pkgs.gdal}/lib"
+      "${pkgs.geos}/lib"
+      "${pkgs.proj}/lib"
+    ];
+
+    # We will create a symlink at $HOME/.local/lib/libgdal.so.30 in the shellHook.
+    # Then we reference it with these environment variables.
+    GDAL_LIBRARY_NONDJANGO_PATH = "$HOME/.local/lib/libgdal.so.30";
+    GDAL_LIBRARY_PATH = "$HOME/.local/lib/libgdal.so.30";
+    GEOS_LIBRARY_PATH = "${pkgs.geos}/lib/libgeos_c.so";
+
+    GDAL_DATA = "${pkgs.gdal}/share/gdal";
+    PROJ_LIB = "${pkgs.proj}/share/proj";
+  };
+
+  shellHook = ''
+    # Create the local library directory if it doesn't exist.
+    mkdir -p $HOME/.local/lib
+
+    # Create (or update) the symlink pointing to the actual GDAL shared library from the nix store.
+    ln -sf ${pkgs.gdal}/lib/libgdal.so $HOME/.local/lib/libgdal.so.30
+
+    echo "Symlink ensured: $HOME/.local/lib/libgdal.so.30 -> ${pkgs.gdal}/lib/libgdal.so"
+  '';
 }
